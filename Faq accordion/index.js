fc8a9b6784f0189fd923c650ed4858787c09b0f2
Michael-mikeojj <mikeojj.personal@gmail.com>
2024-09-29 15:59:55 +0100

    Initial commit
    Firsrt code

---
+++ Faq accordion/index.js
@@ -0,0 +1,20 @@
+const faqToggleBtn = document.querySelectorAll(".answer-toggle-btn");
+const faqAnswer = document.querySelectorAll(".answer");
+
+faqToggleBtn.forEach((button, index) => {
+    button.addEventListener("click", () => {
+        if (button.value === "open") {
+            faqAnswer[index].classList.toggle("hidden");
+            button.innerHTML = `
+                <img src="./assets/images/icon-plus.svg" class="btn-img">
+            `;
+            button.value = "close";
+        } else if (button.value === "closed") {
+            faqAnswer[index].classList.toggle("hidden");
+            button.innerHTML =`
+                <img src="./assets/images/icon-minus.svg" class="btn-img">
+            `;
+            button.value = "open"
+        };
+    });
+});
