942b08f293507d8b5191b839950d6982f9a3fb9d
Michael-mikeojj <mikeojj.personal@gmail.com>
2024-10-09 11:16:50 +0100

    Cleared JavaScript

--- product-list-with-cart-main/index.js
+++ product-list-with-cart-main/index.js
@@ -1,34 +1,106 @@
-const addToCartBtn = document.querySelectorAll(".add-to-cart");
-const dessertImages = document.querySelectorAll(".dessert-img");
-const cart = document.getElementById("cart");
-let cartItems = 0; //for total number of items in cart
-let orderTotal = 0; // for total cost of items
+const desserts = [
+    { 
+       "image": {
+            "thumbnail": "./assets/images/image-waffle-thumbnail.jpg",
+            "mobile": "./assets/images/image-waffle-mobile.jpg",
+            "tablet": "./assets/images/image-waffle-tablet.jpg",
+            "desktop": "./assets/images/image-waffle-desktop.jpg"
+       },
+       "name": "Waffle with Berries",
+       "category": "Waffle",
+       "price": 6.50
+    },
+    {
+        "image": {
+            "thumbnail": "./assets/images/image-creme-brulee-thumbnail.jpg",
+            "mobile": "./assets/images/image-creme-brulee-mobile.jpg",
+            "tablet": "./assets/images/image-creme-brulee-tablet.jpg",
+            "desktop": "./assets/images/image-creme-brulee-desktop.jpg"
+        },
+        "name": "Vanilla Bean Crème Brûlée",
+        "category": "Crème Brûlée",
+        "price": 7.00
+     },
+     {
+        "image": {
+            "thumbnail": "./assets/images/image-macaron-thumbnail.jpg",
+            "mobile": "./assets/images/image-macaron-mobile.jpg",
+            "tablet": "./assets/images/image-macaron-tablet.jpg",
+            "desktop": "./assets/images/image-macaron-desktop.jpg"
+        },
+        "name": "Macaron Mix of Five",
+        "category": "Macaron",
+        "price": 8.00
+     },
+     {
+        "image": {
+            "thumbnail": "./assets/images/image-tiramisu-thumbnail.jpg",
+            "mobile": "./assets/images/image-tiramisu-mobile.jpg",
+            "tablet": "./assets/images/image-tiramisu-tablet.jpg",
+            "desktop": "./assets/images/image-tiramisu-desktop.jpg"
+        },
+        "name": "Classic Tiramisu",
+        "category": "Tiramisu",
+        "price": 5.50
+     },
+     {
+        "image": {
+            "thumbnail": "./assets/images/image-baklava-thumbnail.jpg",
+            "mobile": "./assets/images/image-baklava-mobile.jpg",
+            "tablet": "./assets/images/image-baklava-tablet.jpg",
+            "desktop": "./assets/images/image-baklava-desktop.jpg"
+        },
+        "name": "Pistachio Baklava",
+        "category": "Baklava",
+        "price": 4.00
+     },
+     {
+        "image": {
+            "thumbnail": "./assets/images/image-meringue-thumbnail.jpg",
+            "mobile": "./assets/images/image-meringue-mobile.jpg",
+            "tablet": "./assets/images/image-meringue-tablet.jpg",
+            "desktop": "./assets/images/image-meringue-desktop.jpg"
+        },
+        "name": "Lemon Meringue Pie",
+        "category": "Pie",
+        "price": 5.00
+     },
+     {
+        "image": {
+            "thumbnail": "./assets/images/image-cake-thumbnail.jpg",
+            "mobile": "./assets/images/image-cake-mobile.jpg",
+            "tablet": "./assets/images/image-cake-tablet.jpg",
+            "desktop": "./assets/images/image-cake-desktop.jpg"
+        },
+        "name": "Red Velvet Cake",
+        "category": "Cake",
+        "price": 4.50
+     },
+     {
+        "image": {
+            "thumbnail": "./assets/images/image-brownie-thumbnail.jpg",
+            "mobile": "./assets/images/image-brownie-mobile.jpg",
+            "tablet": "./assets/images/image-brownie-tablet.jpg",
+            "desktop": "./assets/images/image-brownie-desktop.jpg"
+        },
+        "name": "Salted Caramel Brownie",
+        "category": "Brownie",
+        "price": 4.50
+     },
+     {
+        "image": {
+            "thumbnail": "./assets/images/image-panna-cotta-thumbnail.jpg",
+            "mobile": "./assets/images/image-panna-cotta-mobile.jpg",
+            "tablet": "./assets/images/image-panna-cotta-tablet.jpg",
+            "desktop": "./assets/images/image-panna-cotta-desktop.jpg"
+        },
+        "name": "Vanilla Panna Cotta",
+        "category": "Panna Cotta",
+        "price": 6.50
+     }
+];
 
-const desserts = JSON.parse();
-
-// function for functionality of add to cart btn
-function clickedAddToCart(button, index) {
-    let dessertCounter = 1; //creates dessert counter for each dessert
-    button.addEventListener("click", () => {
-        button.classList.add("clicked-add-to-cart");//styling of the button
-        button.innerHTML = `
-            <button type="button" class="change-quantity-btn" value="decrement">
-                <img src="assets/images/icon-decrement-quantity.svg" alt="decrement"> <!-- decrement button-->
-            </button>
-            <p>${dessertCounter}</p>
-            <button type="button" class="change-quantity-btn" value="increment">
-                <img src="assets/images/icon-increment-quantity.svg" alt="increment"> <!--increment btn-->
-            </button>
-        `;//when button is clicked new elements pop within it
-        
-        dessertImages[index].classList.add("dessert-img-border");//adds red border to image
-
-        cart.innerHTML =``;//cart
-
-        //functionality of increment and decrement button
-
-    })
-};
-
-
-addToCartBtn.forEach(clickedAddToCart);
+fetch("./data.json")
+    .then(res => res.json())
+    .then(data => console.log(data))
+    .catch(err => console.log(err))
